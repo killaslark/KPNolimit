ae783ac6dc32c8e5181d411a25682e16a194d422
Bobby <kodok.botak12@gmail.com>
2018-05-23 08:29:00 +0700

    add input user and clean the code

--- SimpleWebParser/src/main/scala/Main.scala
+++ SimpleWebParser/src/main/scala/Main.scala
@@ -5,35 +5,49 @@ import play.api.libs.json.{ JsNull, Json, JsString, JsValue }
 
 
 object Main extends App {
-//  print("Masukkan Link berita yang akan dicari : ")
-//  val pageNewsUrl = scala.io.StdIn.readLine()
+  //Input Web Url From User
 
-  //fetch the HTML
+  print("Masukkan Link berita yang akan dicari : ")
+  val pageNewsUrl = scala.io.StdIn.readLine()
+
+  //fetch the HTML to Doc
   val browser = JsoupBrowser()
-  val doc = browser.get("https://www.liputan6.com/news/read/3481953/pan-sepakat-wacana-sby-lahirkan-pemimpin-baru-di-pilpres")
+  val doc = browser.get(pageNewsUrl)
 
-  // fetch author, title and content
+  // Fetch author, title, date and content from doc
+  //author
   var author = (doc >> texts("div span a span")).last
+
+  //title
   var title = (doc >> text("title"))
+
+  //date
   var date = doc >> text("time")
+
+  //content
   var content = doc >> texts(".article-content-body__item-content > p")
+  content = content.dropRight(1) //delete unused content from doc
+
+  //generate content with endline
+  var contentWithSpace : String = ""
+  for {headline <- content} contentWithSpace += headline.toString + "\n"
 
   //Generate Simple Json object for the Content from HTML
   val jsonObject: JsValue = Json.obj(
     "Author" -> author,
     "Title" -> title,
     "date" -> date,
-    "Content" -> content,
+    "Content" -> contentWithSpace,
   )
 
-
-  //printing all items from html
+  //Print all items from html
   println("Author Name : " + author)
   println("Title : " + title)
   println("Tanggal : " + date)
-  println("Content : ")
-  for {headline <- content} println(headline)
+  println("Content : " + "\n" + contentWithSpace)
 
+  //Print jsonObject
+//  println(jsonObject)
 
 
 }
