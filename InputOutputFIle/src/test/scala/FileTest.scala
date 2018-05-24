de48cdc3a9f2950b09fed227c6388faaac70042d
Bobby <kodok.botak12@gmail.com>
2018-05-24 15:17:10 +0700

    add Test Unit with Wordspec style

--- InputOutputFIle/src/test/scala/FileTest.scala
+++ InputOutputFIle/src/test/scala/FileTest.scala
@@ -1,10 +1,25 @@
-import org.scalatest.FlatSpec
+import org.scalatest.exceptions.TableDrivenPropertyCheckFailedException
+import org.scalatest.prop.TableDrivenPropertyChecks
+  import org.scalatest.{FlatSpec, Matchers, PropSpec, WordSpec}
 
-class ParsedWebTest extends FlatSpec {
+class ParsedWebTestwithFlatSpec extends FlatSpec with Matchers  {
   val filepath = "src/main/scala/"
   val filename = "data.txt"
 
   "Isi File Eksternal " should "be Halooo" in {
     assert(File.readFIle(filename,filepath) == "Halooo\n\nNama Saya Bobby\n")
+  }
+}
+
+class ParsedWebTestwithWordSpec extends WordSpec with Matchers  {
+  val filepath = "src/main/scala/"
+  val filename = "data.txt"
+
+  "Isi File Eksternal" when {
+    "WITH INPUT = data.txt" should {
+      "GENERATE OUTPUT = Halooo" in {
+        assert(File.readFIle(filename,filepath) == "Halooo\n\nNama Saya Bobby\n")
+      }
+    }
   }
 }
--- SimpleWebParser/src/test/scala/ParsedWebTest.scala
+++ SimpleWebParser/src/test/scala/ParsedWebTest.scala
@@ -1,6 +1,6 @@
-import org.scalatest.FlatSpec
+import org.scalatest.{ FlatSpec, Matchers, WordSpec}
 
-class ParsedWebTest extends FlatSpec {
+class ParsedWebTestwithFlatSpec extends FlatSpec with Matchers{
 
   val parsedWeb = new ParsedWeb("https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1")
   "Content Title " should "be Bandara Kertajati Bakal Jadi Bandara Terbaik ke-2 di RI - Bisnis Liputan6.com" in {
@@ -17,6 +17,44 @@ class ParsedWebTest extends FlatSpec {
 
   "Part of the Content " should "be Liputan6.com, Majalengka - Bandara Internasional Jawa Barat (BIJB) Kertajati siap melayani penerbangan pesawat dan penumpang. Rencananya" in {
     assert(parsedWeb.getContent contains  "Liputan6.com, Majalengka - Bandara Internasional Jawa Barat (BIJB) Kertajati siap melayani penerbangan pesawat dan penumpang. Rencananya")
+  }
+
+}
+
+class ParsedWebTestWithWordSpec extends WordSpec with Matchers{
+
+  val parsedWeb = new ParsedWeb("https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1")
+
+  "Content Title " when {
+    "WITH INPUT = https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1 " should {
+      "GENERATE OUTPUT = Bandara Kertajati Bakal Jadi Bandara Terbaik ke-2 di RI - Bisnis Liputan6.com" in {
+        assert(parsedWeb.getTitle == "Bandara Kertajati Bakal Jadi Bandara Terbaik ke-2 di RI - Bisnis Liputan6.com")
+      }
+    }
+  }
+
+  "Content Date time " when {
+    "WITH INPUT = https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1 " should {
+      "GENERATE OUTPUT = 24 Mei 2018, 13:04 WIB" in {
+        assert(parsedWeb.getDate == "24 Mei 2018, 13:04 WIB")
+      }
+    }
+  }
+
+  "Content Author Name " when {
+    "WITH INPUT = https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1 " should {
+      "GENERATE OUTPUT = Ilyas Istianur Praditya" in {
+        assert(parsedWeb.getAuthor == "Ilyas Istianur Praditya")
+      }
+    }
+  }
+
+  "Part of the Content " when {
+    "WITH INPUT = https://www.liputan6.com/bisnis/read/3537205/bandara-kertajati-bakal-jadi-bandara-terbaik-ke-2-di-ri?medium=Headline&campaign=Headline_click_1 " should {
+      "CONSIST OUTPUT = Liputan6.com, Majalengka - Bandara Internasional Jawa Barat (BIJB) Kertajati siap melayani penerbangan pesawat dan penumpang. Rencananya" in {
+        assert(parsedWeb.getContent contains  "Liputan6.com, Majalengka - Bandara Internasional Jawa Barat (BIJB) Kertajati siap melayani penerbangan pesawat dan penumpang. Rencananya")
+      }
+    }
   }
 
 }
